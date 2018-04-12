9d5fb8a5917098fc569bcd89d19cb29ecccd1680
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-12 04:24:51 +0000

    automatically backed up by learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,3 +1,10 @@
+groceries = {
+ dairy: ["milk", "yogurt", "cheese"],
+ vegetable: ["carrots", "broccoli", "cucumbers"],
+ meat: ["chicken", "steak", "salmon"],
+ grains: ["rice", "pasta"]
+}
+
 def get_the_keys(groceries)
   groceries.keys
 end
